70debe6055944fe0d67334e67ea86548c0438e0f
Sri Prasanna <k.sriprasanna@gmail.com>
2014-06-04 19:57:33 -0500

    adding necessary files

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-hosts
+vagrant plugin install vagrant-cachier
---
+++ manifests/local-ci.pp
@@ -0,0 +1,7 @@
+node goserver{
+  
+}
+
+node goagent{
+  
+}
